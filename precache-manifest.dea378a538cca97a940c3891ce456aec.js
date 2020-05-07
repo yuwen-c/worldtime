6c78085ae0c767eec6966a5855d053585a92aef0
marine <marine@MarinedeiMac.local>
2020-05-07 16:03:25 +0800

    Updates

---
+++ precache-manifest.dea378a538cca97a940c3891ce456aec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c492eb0cf8921ebe15b0aab9664675f",
+    "url": "/worldtime/index.html"
+  },
+  {
+    "revision": "1fef86776acad39c06e2",
+    "url": "/worldtime/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "b3650b73689168022663",
+    "url": "/worldtime/static/css/main.58c5a1d9.chunk.css"
+  },
+  {
+    "revision": "1fef86776acad39c06e2",
+    "url": "/worldtime/static/js/2.a78bc555.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/worldtime/static/js/2.a78bc555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b3650b73689168022663",
+    "url": "/worldtime/static/js/main.8f1923dd.chunk.js"
+  },
+  {
+    "revision": "46e7da2ea835e6b4bd2b",
+    "url": "/worldtime/static/js/runtime-main.109fff8f.js"
+  }
+]);
